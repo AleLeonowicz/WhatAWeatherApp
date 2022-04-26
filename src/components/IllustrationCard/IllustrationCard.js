902faa6d5ec1e0a7ed9f1e803882685dfde2f393
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-04-26 12:23:09 +0200

    Added a test img to illustration card

--- src/components/IllustrationCard/IllustrationCard.js
+++ src/components/IllustrationCard/IllustrationCard.js
@@ -1,7 +1,13 @@
 import classes from './IllustrationCard.module.scss';
 
+import woman2030 from '../../assets/fashionIcons/woman/2030.jpg';
+
 const IllustrationCard = props => {
-  return <div className={classes.illustrationCard}></div>;
+  return (
+    <div className={classes.illustrationCard}>
+      <img src={woman2030} alt={'woman2030'}></img>
+    </div>
+  );
 };
 
 export default IllustrationCard;
