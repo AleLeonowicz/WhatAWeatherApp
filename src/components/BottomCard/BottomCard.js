9e5a62a0cd13226dca6b4b05a9b45ce626fc24bc
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-05-02 12:12:08 +0200

    Added styles for lage-xlarge screens

--- src/components/BottomCard/BottomCard.js
+++ src/components/BottomCard/BottomCard.js
@@ -13,17 +13,17 @@ const BottomCard = () => {
 
   const responsive = {
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 3000, min: 980 },
       items: 3,
       slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 980, min: 420 },
       items: 2,
       slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 420, min: 0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
