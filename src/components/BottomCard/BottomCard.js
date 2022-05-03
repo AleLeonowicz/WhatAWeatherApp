2141c05885889de09e9e58735c799c9c424e7f81
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-05-03 15:24:03 +0200

    Created styles for small to medium screens

--- src/components/BottomCard/BottomCard.js
+++ src/components/BottomCard/BottomCard.js
@@ -13,17 +13,17 @@ const BottomCard = () => {
 
   const responsive = {
     desktop: {
-      breakpoint: { max: 3000, min: 600 },
+      breakpoint: { max: 3000, min: 420 },
       items: 3,
       slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
-      breakpoint: { max: 600, min: 420 },
+      breakpoint: { max: 420, min: 320 },
       items: 2,
       slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 420, min: 0 },
+      breakpoint: { max: 320, min: 0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
